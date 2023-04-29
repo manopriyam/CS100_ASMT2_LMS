14e1e6b5944bfb69837afd8abfd195a430d40966
kriti <kritia@iitbhilai.ac.in>
2023-04-29 16:23:34 +0530

    Kriti commit 6

--- CS100_LMS/book_search.php
+++ CS100_LMS/book_search.php
@@ -10,14 +10,31 @@
 
     foreach ($lines as $line) {
       $parts=explode('; ', $line);
-      if(isset($bname)) {
-        if($bname==$parts[1]){
+      if (isset($bname))
+      {
+        if ( strcasecmp($bname,$parts[1] == 0 ))           //==0 means book name found
+        {
+          $book=$parts;
           $flag=1;
         }
         else
         echo"The requeseted book does not exist in the library at the moment";
       }
+      else if (isset($aname))
+      {
+        if (strcasecmp($aname, $parts[2] == 0))
+        {
+          $book=$parts;
+          $flag=1;
+        }
+      }
     }
+
+    if ($flag == 1)
+    {
+      
+    }
+
 
    ?>
 </body>
--- CS100_LMS/book_status.php
+++ CS100_LMS/book_status.php
@@ -1,34 +1,48 @@
 <html>
 <body>
   <?php
-    $bid=$_POST["bid"];
+    $bid = $_POST["bid"];
     $lines=file("books.txt");
     $flag=0;
     $flag2=0;
-    $lines2=file("issue_return.txt");
+    $lines2 = file("issue_return.txt");
 
-    foreach ($lines as $line) {
-      $parts=explode('; ', $line);
-      if ($bid==$parts[0]) {
-        $flag=1;
-        foreach($lines2 as $line2) {
-          $parts2=explode('; ', $line2);
-          if($bid==$parts2[0]){
-            if(!$parts2[4]){
-              $flag2=1;
-            }
-          }
+    foreach ( $lines as $line )
+    {
+      $parts = explode('; ', $line);
+      if ( $bid == $parts[0] )
+      {
+        $book_details=$parts;
+        $flag = 1;
+      }
+    }
+
+    foreach ( $lines2 as $line2 )
+    {
+      $parts2 = explode('; ', $line2);
+      if ( $bid == $parts2[0] )
+      {
+        $avail=$parts2;
+        if ( !$parts2[4] )
+        {
+          $flag2=1;
+        }
+        else
+        {
+          $flag2=0;
         }
       }
     }
 
     if($flag2==1) {
       $status= "Unavailable";
-      $sid=$parts2[1];
+      $sid=$avail[1];
+      $dd=$avail[3];
     }
     else {
       $status ="Available";
       $sid=" ";
+      $dd=" ";
     }
 
     if($flag==1){
@@ -42,15 +56,15 @@
           <table>
               <tr>
                   <td style='font-size : 20; font-family : Verdana, Sans-serif' width=225px>Book ID  :</td>
-                  <td style='width : 500px; font-size : 20; font-family : Verdana, Sans-serif'>$parts[0]</td>
+                  <td style='width : 500px; font-size : 20; font-family : Verdana, Sans-serif'>$book_details[0]</td>
               </tr>
               <tr>
                   <td style='font-size : 20; font-family : Verdana, Sans-serif' width=225px>Book Name  :</td>
-                  <td style='width : 500px; font-size : 20; font-family : Verdana, Sans-serif'>$parts[1]</td>
+                  <td style='width : 500px; font-size : 20; font-family : Verdana, Sans-serif'>$book_details[1]</td>
               </tr>
               <tr>
                   <td style='font-size : 20; font-family : Verdana, Sans-serif' width=225px>Author  :</td>
-                  <td style='width : 500px; font-size : 20; font-family : Verdana, Sans-serif'>$parts[2]</td>
+                  <td style='width : 500px; font-size : 20; font-family : Verdana, Sans-serif'>$book_details[2]</td>
               </tr>
               <tr>
                   <td style='font-size : 20; font-family : Verdana, Sans-serif' width=225px>Availability  :</td>
@@ -60,10 +74,16 @@
                   <td style='font-size : 20; font-family : Verdana, Sans-serif' width=225px>Student ID  :</td>
                   <td style='width : 500px; font-size : 20; font-family : Verdana, Sans-serif'>$sid</td>
               </tr>
+              <tr>
+                  <td style='font-size : 20; font-family : Verdana, Sans-serif' width=225px>Due Date :</td>
+                  <td style='width : 500px; font-size : 20; font-family : Verdana, Sans-serif'>$dd</td>
+              </tr>
           </table>
           </div>
         ";
     }
+    else
+    echo"The requested book is not in stock";
    ?>
 </body>
 </html>
