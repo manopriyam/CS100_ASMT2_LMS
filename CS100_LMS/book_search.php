b4ee92d7bb10863913307fa20f94fb04f6965547
kriti <kritia@iitbhilai.ac.in>
2023-04-29 17:36:32 +0530

    Kriti commit 7

--- CS100_LMS/book_search.php
+++ CS100_LMS/book_search.php
@@ -8,34 +8,65 @@
 
     $flag=0;
 
+    echo "<div style='font-size : 30; font-family : Verdana, Sans-serif; padding-left : 30%; padding-bottom : 10%'>
+    <table border='2'>
+        <tr>
+            <td style='width : 125px; font-size : 18; font-family : Verdana, Sans-serif; text-align : center; padding : 2px'>
+                <b>BOOK ID</b>
+            </td>
+            <td style='width : 400px; font-size : 18; font-family : Verdana, Sans-serif; text-align : center; padding : 2px'>
+                <b>BOOK NAME</b>
+            </td>
+            <td style='width : 200px; font-size : 18; font-family : Verdana, Sans-serif; text-align : center; padding : 2px'>
+                <b>AUTHOR NAME</b>
+            </td>
+        </tr>";
+
     foreach ($lines as $line) {
       $parts=explode('; ', $line);
       if (isset($bname))
       {
-        if ( strcasecmp($bname,$parts[1] == 0 ))           //==0 means book name found
+        if ( strcasecmp($bname,$parts[1]) == 0 )           //==0 means book name found
         {
-          $book=$parts;
-          $flag=1;
+          echo"
+          <tr>
+              <td style='width : 125px; font-size : 16; font-family : Verdana, Sans-serif; text-align : center; padding : 4px'>
+                  $parts[0]
+              </td>
+              <td style='width : 400px; font-size : 16; font-family : Verdana, Sans-serif; padding : 4px'>
+                  $parts[1]
+              </td>
+              <td style='width : 200px; font-size : 16; font-family : Verdana, Sans-serif; padding : 4px'>
+                  $parts[2]
+              </td>
+          </tr>
+
+      ";
         }
-        else
-        echo"The requeseted book does not exist in the library at the moment";
       }
       else if (isset($aname))
       {
-        if (strcasecmp($aname, $parts[2] == 0))
+        echo"hello";
+        if (strcasecmp($aname, $parts[2]) == 0)
         {
-          $book=$parts;
-          $flag=1;
+          echo"
+          <tr>
+              <td style='width : 125px; font-size : 16; font-family : Verdana, Sans-serif; text-align : center; padding : 4px'>
+                  $parts[0]
+              </td>
+              <td style='width : 400px; font-size : 16; font-family : Verdana, Sans-serif; padding : 4px'>
+                  $parts[1]
+              </td>
+              <td style='width : 200px; font-size : 16; font-family : Verdana, Sans-serif; padding : 4px'>
+                  $parts[2]
+              </td>
+          </tr>
+      ";
         }
       }
     }
-
-    if ($flag == 1)
-    {
-      
-    }
-
-
    ?>
+ </table>
+ </div>
 </body>
 </html>
